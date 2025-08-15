3c44677dda07997582b9648ba57ea707daab6237
dragonenderguy2 <shaihalapre@hotmail.com>
2025-08-15 05:45:17 -0500

    feat: initial commit for src/controllers/dataController.js

---
+++ src/controllers/dataController.js
@@ -0,0 +1,13 @@
+// Contrôleur pour gérer les données
+
+exports.getAllData = (req, res) => {
+  // Logique pour obtenir toutes les données
+};
+
+exports.importData = (req, res) => {
+  // Logique pour importer des données
+};
+
+exports.exportData = (req, res) => {
+  // Logique pour exporter des données
+};
