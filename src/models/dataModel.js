595b6c0dfd51ec01b9f38002fa5146c73f4c1923
dragonenderguy2 <shaihalapre@hotmail.com>
2025-08-15 05:45:19 -0500

    feat: initial commit for src/models/dataModel.js

---
+++ src/models/dataModel.js
@@ -0,0 +1,10 @@
+// Modèle de données
+
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  value: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Data', dataSchema);
